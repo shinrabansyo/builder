f2701c10cc84152390eb3cc0162fc3ec8a891a33
NakagamiYuta <guguru0014@gmail.com>
2025-05-25 23:21:20 +0900

    [change] sb-assembler を直接呼び出す形でビルドを行うようにした

--- src/command/utils/build.rs
+++ src/command/utils/build.rs
@@ -1,24 +1,34 @@
 use std::fs;
-
-use sb_asm::assemble;
+use std::process::Command as StdCommand;
 
 use crate::config::build::InputType;
 use crate::config::Config;
 
-pub fn build(config: &Config) -> anyhow::Result<(String, String)> {
+pub fn build(config: &Config) -> anyhow::Result<()> {
     // 1. src/main.asm をチェック
     match &config.build.input {
-        InputType::Asm => {
-            if !fs::exists("./src/main.asm")? {
-                return Err(anyhow::anyhow!("src/main.asm not found."));
-            }
+        InputType::Asm => if !fs::exists("./src/main.asm")? {
+            return Err(anyhow::anyhow!("src/main.asm not found."));
         }
     }
 
     // 2. ビルド
     match &config.build.input {
-        InputType::Asm => {
-            assemble(&fs::read_to_string("./src/main.asm")?)
-        }
+        InputType::Asm => build_asm(config),
     }
+}
+
+fn build_asm(_: &Config) -> anyhow::Result<()> {
+    // 1. target 準備
+    let _ = fs::remove_dir("target/build");
+    fs::create_dir_all("target/build")?;
+
+    // 2. アセンブル
+    StdCommand::new("sb-assembler")
+        .arg("./src/main.asm")
+        .arg("./target/build/data.hex")
+        .arg("./target/build/inst.hex")
+        .status()?;
+
+    Ok(())
 }
