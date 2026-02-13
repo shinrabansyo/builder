8ecdc904ef3f8dddda660aad32febdac121d853b
NakagamiYuta <guguru0014@gmail.com>
2026-02-13 16:52:31 +0900

    [update] meta-config のパスを実行時に指定できるようにした

--- src/command.rs
+++ src/command.rs
@@ -1,13 +1,64 @@
-pub mod build;
-pub mod info;
-pub mod init;
-pub mod new;
-pub mod run;
-pub mod oneshot;
+mod build;
+mod info;
+mod init;
+mod new;
+mod run;
+mod oneshot;
 mod utils;
+
+use bpaf::Bpaf;
 
 use crate::config_meta::MetaConfig;
 
+use build::{Build, build};
+use info::{Info, info};
+use init::{Init, init};
+use new::{New, new};
+use oneshot::{Oneshot, oneshot};
+use run::{Run, run};
+
 pub trait Runnable {
     fn run(self, meta_config: MetaConfig) -> anyhow::Result<()>;
+}
+
+#[derive(Debug, Bpaf)]
+pub struct Command {
+    // プロジェクト管理系
+    #[bpaf(external, optional)]
+    new: Option<New>,
+    #[bpaf(external, optional)]
+    init: Option<Init>,
+    #[bpaf(external, optional)]
+    info: Option<Info>,
+
+    // ビルド・実行系
+    #[bpaf(external, optional)]
+    build: Option<Build>,
+    #[bpaf(external, optional)]
+    run: Option<Run>,
+
+    // その他
+    #[bpaf(external, optional)]
+    oneshot: Option<Oneshot>,
+}
+
+impl Runnable for Command {
+    fn run(self, meta_config: MetaConfig) -> anyhow::Result<()> {
+        macro_rules! run_cmd {
+            ($cmd:expr) => {
+                if let Some(cmd) = $cmd {
+                    return cmd.run(meta_config);
+                }
+            };
+        }
+
+        run_cmd!(self.info);
+        run_cmd!(self.new);
+        run_cmd!(self.init);
+        run_cmd!(self.build);
+        run_cmd!(self.run);
+        run_cmd!(self.oneshot);
+
+        Err(anyhow::anyhow!("No command specified. Use --help to see available commands."))
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,62 +2,30 @@ mod command;
 mod config_meta;
 mod config_project;
 
+use std::path::PathBuf;
+
 use bpaf::Bpaf;
 
-use command::build::{Build, build};
-use command::info::{Info, info};
-use command::init::{Init, init};
-use command::new::{New, new};
-use command::oneshot::{Oneshot, oneshot};
-use command::run::{Run, run};
-use command::Runnable;
+use command::{Command, Runnable, command};
 use config_meta::MetaConfig;
 
-#[derive(Debug, Clone, Bpaf)]
+#[derive(Debug, Bpaf)]
 #[bpaf(options, version)]
 struct CliOptions {
-    // プロジェクト管理系
-    #[bpaf(external, optional)]
-    new: Option<New>,
-    #[bpaf(external, optional)]
-    init: Option<Init>,
-    #[bpaf(external, optional)]
-    info: Option<Info>,
+    /// Path to the meta configuration file (default: '.sb-builder/Config.toml')
+    #[bpaf(long, short, fallback("./.sb-builder/Config.toml".into()))]
+    meta_config: PathBuf,
 
-    // ビルド・実行系
-    #[bpaf(external, optional)]
-    build: Option<Build>,
-    #[bpaf(external, optional)]
-    run: Option<Run>,
-
-    // その他
-    #[bpaf(external, optional)]
-    oneshot: Option<Oneshot>,
-}
-
-impl Runnable for CliOptions {
-    fn run(self, meta_config: MetaConfig) -> anyhow::Result<()> {
-        macro_rules! run_cmd {
-            ($cmd:expr) => {
-                if let Some(cmd) = $cmd {
-                    return cmd.run(meta_config);
-                }
-            };
-        }
-
-        run_cmd!(self.info);
-        run_cmd!(self.new);
-        run_cmd!(self.init);
-        run_cmd!(self.build);
-        run_cmd!(self.run);
-        run_cmd!(self.oneshot);
-
-        Err(anyhow::anyhow!("No command specified. Use --help to see available commands."))
-    }
+    /// Subcommand to execute
+    #[bpaf(external)]
+    command: Command,
 }
 
 fn main() -> anyhow::Result<()> {
-    cli_options()
-        .run()
-        .run(MetaConfig::load_or_default("./.sb-builder/Config.toml")?)
+    let opts = cli_options()
+        .header("A build and run tool for Shinrabansyo Project")
+        .usage("Usage: sb_builder_cli [-m=ARG] [COMMAND ...] ")
+        .run();
+    let meta_config = MetaConfig::load_or_default(&opts.meta_config)?;
+    opts.command.run(meta_config)
 }
